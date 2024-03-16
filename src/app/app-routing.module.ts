627988d54781e62fe762a8ad3c386b73ea07caaa
LoicLoadcontrol <jacques_loic@funkyduck.be>
2024-03-16 15:49:46 +0100

    structurate project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {LoginComponent} from "./components/login/login.component";
+import { RegisterComponent } from './components/register/register.component';
+import {Error404Component} from "./components/error404/error404.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'register',component: RegisterComponent},
+  {path: 'home', component: HomeComponent},
+  {path: '**', component: Error404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { MapComponent } from './components/map/map.component';
+import { ListComponent } from './components/list/list.component';
+import { FilterComponent } from './components/filter/filter.component';
+import { GiveComponent } from './components/give/give.component';
+import { ProfilComponent } from './components/profil/profil.component';
+import { GroupsComponent } from './components/groups/groups.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { Error404Component } from './components/error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    MapComponent,
+    ListComponent,
+    FilterComponent,
+    GiveComponent,
+    ProfilComponent,
+    GroupsComponent,
+    NavbarComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
