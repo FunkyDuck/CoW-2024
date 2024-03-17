12b13871b88a777419d80ff3e99c3474383e5433
LoicLoadcontrol <jacques_loic@funkyduck.be>
2024-03-17 11:35:16 +0100

    add product with geocoord

--- src/app/components/give/give.component.ts
+++ src/app/components/give/give.component.ts
@@ -1,5 +1,6 @@
 import { Component, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
+import {FoodsService} from "../../services/foods.service";
 
 @Component({
   selector: 'app-give',
@@ -8,11 +9,13 @@ import * as L from 'leaflet';
 })
 export class GiveComponent implements AfterViewInit {
   private map: any;
+  lat: any = null;
+  lon: any = null;
 
   private initMap(): void {
     this.map = L.map('map-mini', {
-      center: [ 39.8282, -98.5795 ],
-      zoom: 3
+      center: [ 50.83369767098071, 4.528767076882302 ],
+      zoom: 5
     });
 
     const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -21,12 +24,34 @@ export class GiveComponent implements AfterViewInit {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
 
+    this.map.on('click', (e: any) =>{
+      console.log(e.latlng)
+      this.lat = e.latlng.lat;
+      this.lon = e.latlng.lng;
+    });
+
     tiles.addTo(this.map);
   }
 
-  constructor() {}
+  constructor(private _food: FoodsService) {}
 
   ngAfterViewInit(): void {
     this.initMap();
+  }
+
+  sendOffer(): void {
+    let name = (document.getElementById('name')as any).value;
+    let description = (document.getElementById('description')as any).value;
+    let food_category_id = (document.getElementById('food_category_id')as any).value;
+    let diet = (document.getElementById('diet')as any).value;
+
+    this._food.sendProduct({name: name, description: description, food_category_id: food_category_id, diet: diet, lat: this.lat, lon: this.lon}).subscribe(
+      (res: any)=>{
+        console.log(res);
+      },
+      (err: any)=>{
+        console.error(err);
+      }
+    );
   }
 }
--- src/app/services/foods.service.ts
+++ src/app/services/foods.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class FoodsService {
+  url: string = 'https://api.studshareat.com/api/';
 
-  constructor() { }
+  httpOptions() {
+    return {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem('access_token')
+      })
+    }
+  };
+
+  constructor(private http: HttpClient) { }
+
+  sendProduct(product: any) {
+    return this.http.post(this.url + 'food',product, this.httpOptions());
+  }
 }
